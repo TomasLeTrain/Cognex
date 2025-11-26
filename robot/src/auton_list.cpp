e2e6d108434fee4818fdcf6d0bfb78e69d85b245
TomasLeTrain <tomylopezd@gmail.com>
2025-11-26 08:06:17 -0500

    attempt to improve code size, down to 200k (untested)

--- robot/src/auton_list.cpp
+++ robot/src/auton_list.cpp
@@ -4,18 +4,20 @@
 
 // clang-format off
 std::unordered_map<std::string, std::function<void()>> auton_list = {
-	L_AUTON(roboticon_quals, "roboticon quals")
-	
-    L_AUTON(disabled_auton, "disabled")
+	// commented out ununsed autons to save on compile size
 
-    L_AUTON(simple_auton, "simple")
-	L_AUTON(skills, "old skills")
-    L_AUTON(skills2, "new sklls")
-    L_AUTON(simple_other_goal_auton, "simple other goal")
-    L_AUTON(roboticon_skills, "roboticon skills")
-
-	L_AUTON(eagles_skills, "eagles skills")
-	L_AUTON(eagles_quals, "eagles quals")
+	// L_AUTON(roboticon_quals, "roboticon quals")
+	//
+	//    L_AUTON(disabled_auton, "disabled")
+	//
+	//    L_AUTON(simple_auton, "simple")
+	// L_AUTON(skills, "old skills")
+	//    L_AUTON(skills2, "new sklls")
+	//    L_AUTON(simple_other_goal_auton, "simple other goal")
+	//    L_AUTON(roboticon_skills, "roboticon skills")
+	//
+	// L_AUTON(eagles_skills, "eagles skills")
+	// L_AUTON(eagles_quals, "eagles quals")
 
 	L_AUTON(sunshine_skills, "current skills")
 
