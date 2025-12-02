07e7cbacde932037a25e6934265cfa00f77dcbad
TomasLeTrain <tomylopezd@gmail.com>
2025-12-02 17:56:39 -0500

    updated libs

--- include/blazing/motions/boomerang.hpp
+++ include/blazing/motions/boomerang.hpp
@@ -138,7 +138,7 @@ class boomerang : public Motion<ControllersType,
                 state.crossed_sideways = true;
 
                 // return carrot;
-                // if crossed we likley just want mtp behavior
+                // if crossed we likely just want mtp behavior
                 return target;
             }
 
