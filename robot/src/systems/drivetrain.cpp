e39645329bc2b9be5bcad70a94b74b67ea7b6756
TomasLeTrain <tomylopezd@gmail.com>
2025-12-05 22:33:53 -0500

    small cleanup, add ability to disable odom on vexmaps

--- robot/src/systems/drivetrain.cpp
+++ robot/src/systems/drivetrain.cpp
@@ -12,9 +12,9 @@ bool started = false;
 void driveUpdate() {
   // get left y and right x positions
 
-  float linear_deadband = 3;
-  float linear_curveGain = 1.019;
-  float linear_minOutput = 10;
+  // float linear_deadband = 3;
+  // float linear_curveGain = 1.019;
+  // float linear_minOutput = 10;
 
   float angular_deadband = 3;
   float angular_curveGain = 1.019;
@@ -53,15 +53,6 @@ void driveUpdate() {
 
   left_motors.move(leftPower);
   right_motors.move(rightPower);
-
-  // Voltage dir = from_volt(
-  //   controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) / 127.0);
-  // Voltage turn = -from_volt(
-  //   controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X) / 127.0);
-  //
-  // dir = driver_linear_slew.linear_slew.apply(dir, 10_msec);
-  //
-  // drivetrain.moveArcade(dir, turn);
 }
 
 void init() {
